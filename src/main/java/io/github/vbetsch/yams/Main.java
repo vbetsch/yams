1882882dc238291623c233f1203c57b6816aa491
vbetsch <71458556+vbetsch@users.noreply.github.com>
2026-09-25 20:55:34 +0200

    refactor: clean base template code

--- src/main/java/io/github/vbetsch/yams/Main.java
+++ src/main/java/io/github/vbetsch/yams/Main.java
@@ -2,8 +2,6 @@ package io.github.vbetsch.yams;
 
 public class Main {
     static void main() {
-        CodeCracker codeCracker = new CodeCracker();
         IO.println("Hello world!");
-        System.out.println("Encrypted : " + codeCracker.encryptString("Hello world!"));
     }
 }
