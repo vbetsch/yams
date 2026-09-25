00f4c98baeebedfdd6ea1844e916fa8c5bfe0fbc
vbetsch <71458556+vbetsch@users.noreply.github.com>
2026-09-25 21:16:40 +0200

    test: should return 40 points

---
+++ src/test/java/io/github/vbetsch/yams/YamsTest.java
@@ -0,0 +1,21 @@
+package io.github.vbetsch.yams;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class YamsTest {
+    @Test
+    void should_return40Points() {
+        // Arrange
+        Yams yams  = new Yams();
+
+        // Act
+        int result = yams.computeScore(List.of(1, 2, 3, 4, 5), "LARGE_STRAIGHT");
+
+        // Assert
+        assertEquals(40, result);
+    }
+}
