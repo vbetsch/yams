650315673c1e67beec5250378edc2ea84ef73691
vbetsch <71458556+vbetsch@users.noreply.github.com>
2026-09-25 21:22:57 +0200

    feat: create computeScore method

---
+++ src/main/java/io/github/vbetsch/yams/Yams.java
@@ -0,0 +1,9 @@
+package io.github.vbetsch.yams;
+
+import java.util.List;
+
+public class Yams {
+    public int computeScore(List<Integer> combination, String category) {
+        return 40;
+    }
+}
