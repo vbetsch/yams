ec4d592d323c8269bb6077a5aed4155b6451138d
vbetsch <71458556+vbetsch@users.noreply.github.com>
2026-09-25 23:01:58 +0200

    refactor: split categories cases in private methods

--- src/main/java/io/github/vbetsch/yams/Yams.java
+++ src/main/java/io/github/vbetsch/yams/Yams.java
@@ -4,25 +4,32 @@ import java.util.List;
 import java.util.stream.IntStream;
 
 public class Yams {
+    private int handleChanceScore(List<Integer> combination) {
+        int result = 0;
+        for (Integer integer : combination) {
+            result += integer;
+        }
+        return result;
+    }
+
+    private int handleYamsScore(List<Integer> combination) {
+        IntStream distinctedCombination = combination
+                .stream()
+                .mapToInt(Integer::intValue)
+                .distinct();
+        if (distinctedCombination.count() == 1) {
+            IO.println("ITS A YAMS!!! GOOD GAME !!!");
+            return 50;
+        } else {
+            return 0;
+        }
+    }
+
     public int computeScore(List<Integer> combination, CategoryEnum category) {
         if (category == CategoryEnum.CHANCE) {
-            int result = 0;
-            for (Integer integer : combination) {
-                result += integer;
-            }
-            return result;
+            return this.handleChanceScore(combination);
         } else if (category == CategoryEnum.YAMS) {
-            IntStream distinctedCombination = combination
-                    .stream()
-                    .mapToInt(Integer::intValue)
-                    .distinct();
-            if (distinctedCombination.count() == 1) {
-                IO.println("ITS A YAMS!!! GOOD GAME");
-                return 50;
-            } else {
-                IO.println("SOOO BAAAAD !");
-                return 0;
-            }
+            return this.handleYamsScore(combination);
         }
         return 1000;
     }
