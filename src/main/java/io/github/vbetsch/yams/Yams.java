16ea2ce927fbbd045a0da0514c558401472d4f42
vbetsch <71458556+vbetsch@users.noreply.github.com>
2026-09-25 22:52:37 +0200

    feat: compute score for combination of five 6

--- src/main/java/io/github/vbetsch/yams/Yams.java
+++ src/main/java/io/github/vbetsch/yams/Yams.java
@@ -1,6 +1,7 @@
 package io.github.vbetsch.yams;
 
 import java.util.List;
+import java.util.stream.IntStream;
 
 public class Yams {
     public int computeScore(List<Integer> combination, CategoryEnum category) {
@@ -10,6 +11,18 @@ public class Yams {
                 result += integer;
             }
             return result;
+        } else if (category == CategoryEnum.YAMS) {
+            IntStream distinctedCombination = combination
+                    .stream()
+                    .mapToInt(Integer::intValue)
+                    .distinct();
+            if (distinctedCombination.count() == 1) {
+                IO.println("ITS A YAMS!!! GOOD GAME");
+                return 50;
+            } else {
+                IO.println("SOOO BAAAAD !");
+                return 0;
+            }
         }
         return 1000;
     }
